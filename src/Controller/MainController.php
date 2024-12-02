613ca7a5d4662d14b3cfb4d23a2d9e135edb3038
BeniaminModrzynski <b.modrzynski@edu.bbq.de>
2024-12-02 15:34:33 +0100

    response bearbeitet, twig erstellt mit fallunterscheidung

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,15 +1,25 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function letsgo(): Response
     {
-        return new Response("Party!");
+        $party1 = ['name'=>'Techno', 'seats'=> 100]; # Fuer die Techno Party sind noch 100 Plätze frei
+        $party2 = ['name'=>'Rock', 'seats'=> 10]; # Achtung fuer die Rock Party sind nur noch 10 Plätze frei
+        $party3 = ['name'=>'Hip Hop', 'seats'=> 0]; # Die Hip Hop Party Ausverkauft!
+
+        $partys = [$party1, $party2, $party3];
+
+        $daten = ['name'=>'Hip Hop Party' , 'partys'=>$partys];
+
+        return $this->render('main/letsgo.html.twig', $daten);
+//        return $this->render('main/letsgo.html.twig', ['name'=>'Hip Hop Party']);
     }
 
     #[Route('/show/{slug}')]
