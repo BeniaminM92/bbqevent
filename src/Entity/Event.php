c2d0a85c200c62617874574c11cd471ca992ce8e
BeniaminModrzynski <b.modrzynski@edu.bbq.de>
2024-12-06 08:35:44 +0100

    - ORM eingebunden - Entitys erzeugt , Event - EvenController new methode erstellt - .env.local erstellt mit DB verbindung

---
+++ src/Entity/Event.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $bookedseats = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getBookedseats(): ?int
+    {
+        return $this->bookedseats;
+    }
+
+    public function setBookedseats(int $bookedseats): static
+    {
+        $this->bookedseats = $bookedseats;
+
+        return $this;
+    }
+}
