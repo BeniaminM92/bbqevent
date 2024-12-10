c0ab470cc4aa837d796f04025cc5f99202ae42c3
BeniaminModrzynski <b.modrzynski@edu.bbq.de>
2024-12-10 15:34:26 +0100

    - FormTypes Location und Event - twigs erweitert - Validator implementiert - Methoden CRUD in Event und Location Controller erweitert & angepasst

--- src/Entity/Location.php
+++ src/Entity/Location.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\LocationRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: LocationRepository::class)]
 class Location
@@ -13,6 +14,7 @@ class Location
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank(message: "Location Name darf nicht Leer sein!")]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
