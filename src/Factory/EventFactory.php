7f73c3db280f57ed25ea4c4bbe4e2db7820a66ed
BeniaminModrzynski <b.modrzynski@edu.bbq.de>
2024-12-16 12:24:31 +0100

    - fixtures und factory eingebunden - dummy datensätze erzeugt

---
+++ src/Factory/EventFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Event;
+use App\Entity\EventTypeEnum;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Event>
+ */
+final class EventFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Event::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'bookedseats' => self::faker()->numberBetween(50,3000),
+            'description' => self::faker()->text(255),
+            'name' => self::faker()->userName(),
+            'eventtype' => self::faker()->randomElement(EventTypeEnum::cases()),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Event $event): void {})
+        ;
+    }
+}
---
+++ src/Factory/LocationFactory.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Location;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Location>
+ */
+final class LocationFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Location::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'address' => self::faker()->address,
+            'email' => self::faker()->companyEmail,
+            'name' => self::faker()->city(),
+            'capacity' => self::faker()->numberBetween(100, 10000),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Location $location): void {})
+        ;
+    }
+}
